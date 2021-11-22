5bf582c028ab28304041905c523352843ed9c095
SandraQS <sanquesan@gmail.com>
2021-11-23 00:12:47 +0100

    Created index db and server

---
+++ server/index.ts
@@ -0,0 +1,39 @@
+const cors = require("cors");
+const morgan = require("morgan");
+const debug = require("debug")("UniteTattoo:server");
+const express = require("express");
+const chalk = require("chalk");
+const {
+  handlerNotFound,
+  handlerGeneralError,
+} = require("./middlewares/errors");
+
+const app = express();
+
+const initServer = (port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.yellow(`Escuchando en el puerto ${port}`));
+      resolve(server);
+    });
+
+    server.on("error", (error) => {
+      debug(chalk.red("Error al iniciar el servidor"));
+      if (error.code === "EADDRINUSE") {
+        debug(chalk.red(`El puerto ${port} está ocupado`));
+      }
+      reject();
+    });
+
+    server.on("close", () => {
+      debug(chalk.blue(`El servidor se ha desconectado`));
+    });
+  });
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+app.use(handlerNotFound);
+app.use(handlerGeneralError);
+export = { initServer, app };
---
+++ server/middlewares/errors.js
@@ -0,0 +1,18 @@
+const debug = require("debug")("UniteTattoo:errors");
+const chalk = require("chalk");
+
+const handlerNotFound = (req, res) => {
+  res.status(404).json({ error: "No se ha encontrado la ruta" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const handlerGeneralError = (error, req, res, next) => {
+  debug(chalk.red("Ha ocurrido un error: ", error.message));
+  const message = error.code ? error.message : "ERROR";
+  res.status(error.code || 500).json({ error: message });
+};
+
+module.exports = {
+  handlerNotFound,
+  handlerGeneralError,
+};
