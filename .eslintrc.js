962d7ca48aeb259c1953eca63160d090724eea4d
SandraQS <sanquesan@gmail.com>
2021-11-22 18:38:15 +0100

    Add rules and change rute project at eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,24 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "airbnb-typescript/base", "airbnb-typescript"],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "airbnb-typescript",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    project: tsconfig.json,
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
 };
