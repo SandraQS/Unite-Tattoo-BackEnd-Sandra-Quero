b5335b679a11754e641070bbd421c5fe613e1053
SandraQS <sanquesan@gmail.com>
2021-11-22 18:23:35 +0100

    Chnge extends and parseOptions at eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["airbnb-base", "airbnb-typescript/base"],
+  extends: ["airbnb-base", "airbnb-typescript/base", "airbnb-typescript"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
+    project: tsconfig.json,
   },
   plugins: ["@typescript-eslint"],
   rules: {},
