cf12b1efb9db3d49a50b950b729b4b5d4879b2ca
SandraQS <sanquesan@gmail.com>
2021-11-23 20:04:59 +0100

    Created tattooArtist controller

--- server/controllers/tattooArtistController.ts
+++ server/controllers/tattooArtistController.ts
@@ -1,22 +1,42 @@
+import bcrypt from "bcrypt";
+import express from "express";
 import TattooArtistModel from "../../database/models/tattooArtist";
 
-const tattooArtistRegister = async (req, res) => {
-  const {
-    personalDataTattoArtist,
-    userDataTattoArtist,
-    professionalDataTattooArtist,
-    collections,
-    appointmentSchedule,
-  } = req.body;
+class CodeError extends Error {
+  code: number | undefined;
+}
 
-  const newTattooArtist = await TattooArtistModel.create({
-    personalDataTattoArtist,
-    userDataTattoArtist,
-    professionalDataTattooArtist,
-    collections,
-    appointmentSchedule,
-  });
-  res.json(newTattooArtist);
+const tattooArtistRegister = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const {
+      personalDataTattoArtist,
+      userDataTattoArtist: { password, userName, email },
+      professionalDataTattooArtist,
+      collections,
+      appointmentSchedule,
+    } = req.body;
+
+    const newTattooArtist = await TattooArtistModel.create({
+      personalDataTattoArtist,
+      userDataTattoArtist: {
+        userName,
+        password: await bcrypt.hash(password, 10),
+        email,
+      },
+      professionalDataTattooArtist,
+      collections,
+      appointmentSchedule,
+    });
+    res.status(201).json(newTattooArtist);
+  } catch {
+    const error = new CodeError("Objeto no válido");
+    error.code = 404;
+    next(error);
+  }
 };
 
 export default tattooArtistRegister;
