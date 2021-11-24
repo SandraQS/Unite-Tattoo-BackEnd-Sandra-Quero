6895f584a162547f0a0f230e330887d1051baaa6
SandraQS <sanquesan@gmail.com>
2021-11-24 16:19:04 +0100

    Created editCollection function

--- server/routes/tattooArtist/collectionsRoutes.ts
+++ server/routes/tattooArtist/collectionsRoutes.ts
@@ -3,6 +3,7 @@ import {
   createCollection,
   getCollections,
   deleteCollection,
+  editCollection,
 } from "../../controllers/collectionsController";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/collection/create", createCollection);
 router.get("/collection", getCollections);
 router.delete("/collection/delete/:id", deleteCollection);
+router.put("/collection/edit/:idCollection", editCollection);
 
 export default router;
