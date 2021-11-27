5c69b8d4423d738942d95d7f5e2474e112a8af33
SandraQS <sanquesan@gmail.com>
2021-11-27 12:34:49 +0100

    Create test for /uniteTattoo/tattooArtist/register route

--- server/routes/tattooArtist/usersRoutes.test.ts
+++ server/routes/tattooArtist/usersRoutes.test.ts
@@ -1,69 +1,141 @@
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import supertest from "supertest";
-// import chalk from "chalk";
-// import initDB from "../../../database";
-// import TattooArtistModel from "../../../database/models/tattooArtistModel";
-// import { initServer, app } from "../..";
+import dotenv from "dotenv";
 
-// const request = supertest(app);
+dotenv.config();
+import mongoose from "mongoose";
+import supertest from "supertest";
 
-// let server;
+import { initServer, app } from "../..";
+import initDB from "../../../database";
 
-// beforeAll(async () => {
-//   await initDB(process.env.MONGODB_STRING_UNITETATTOO_TESTS);
-//   server = await initServer(process.env.SERVER_PORT_TESTS);
-//   await TattooArtistModel.deleteMany({});
-// });
+import TattooArtistModel from "../../../database/models/tattooArtistModel";
 
-// afterAll((done) => {
-//   server.close(async () => {
-//     await mongoose.connection.close();
-//     done();
-//   });
-// });
+const request = supertest(app);
+jest.setTimeout(20000);
 
-// beforeEach(async () => {
-//   await TattooArtistModel.create({
-//     personalDataTattoArtist: {
-//       name: "Gisela",
-//       surname1: "Quero",
-//       surname2: "Sánchez",
-//     },
-//     userDataTattoArtist: {
-//       userName: "ShivaShana",
-//       password: "hola",
-//       email: "email@gmail.com",
-//     },
-//     professionalDataTattooArtist: {
-//       studioName: "Shiva",
-//       professionalName: "ShivaShana",
-//       phone: 666666666,
-//       contactEmail: "email@gmail.com",
-//       openingHours: "de 9.00 a 18.00h",
-//       direction: "C/hola, nº13",
-//       colaboration: "true",
-//     },
-//   });
-//   await TattooArtistModel.create({
-//     personalDataTattoArtist: {
-//       name: "Gisela",
-//       surname1: "Quero",
-//       surname2: "Sánchez",
-//     },
-//     userDataTattoArtist: {
-//       userName: "ShivaShana",
-//       password: "hola",
-//       email: "email@gmail.com",
-//     },
-//     professionalDataTattooArtist: {
-//       studioName: "Shiva",
-//       professionalName: "ShivaShana",
-//       phone: 666666666,
-//       contactEmail: "email@gmail.com",
-//       openingHours: "de 9.00 a 18.00h",
-//       direction: "C/hola, nº13",
-//       colaboration: "true",
-//     },
-//   });
-// });
+let server;
+
+beforeAll(async () => {
+  await initDB(process.env.MONGODB_STRING_UNITETATTOO_TESTS);
+  server = await initServer(8909);
+});
+
+afterAll((done) => {
+  server.close(async () => {
+    await mongoose.connection.close();
+    done();
+  });
+});
+
+beforeEach(async () => {
+  // await TattooArtistModel.deleteMany({});
+  await TattooArtistModel.create({
+    personalDataTattoArtist: {
+      name: "Gisela",
+      surname1: "Quero",
+      surname2: "Sánchez",
+    },
+    userDataTattoArtist: {
+      userName: "ShivaShana",
+      password: "hola",
+      email: "email@gmail.com",
+    },
+    professionalDataTattooArtist: {
+      studioName: "Shiva",
+      professionalName: "ShivaShana",
+      phone: 666666666,
+      contactEmail: "email@gmail.com",
+      openingHours: "de 9.00 a 18.00h",
+      direction: "C/hola, nº13",
+      colaboration: "true",
+    },
+  });
+  await TattooArtistModel.create({
+    personalDataTattoArtist: {
+      name: "Gisela",
+      surname1: "Quero",
+      surname2: "Sánchez",
+    },
+    userDataTattoArtist: {
+      userName: "ShivaShana",
+      password: "hola",
+      email: "email@gmail.com",
+    },
+    professionalDataTattooArtist: {
+      studioName: "Shiva",
+      professionalName: "ShivaShana",
+      phone: 666666666,
+      contactEmail: "email@gmail.com",
+      openingHours: "de 9.00 a 18.00h",
+      direction: "C/hola, nº13",
+      colaboration: "true",
+    },
+  });
+});
+
+describe("Given /userRoutes route", () => {
+  describe("When it receives a POST request to the /register route with a req.body correct", () => {
+    test("Then it should respond a with the new tattooArtist and a 201 status", async () => {
+      const { body } = await request
+        .post("/uniteTattoo/tattooArtist/register")
+        .send({
+          personalDataTattoArtist: {
+            name: "Gisela",
+            surname1: "Quero",
+            surname2: "Sánchez",
+          },
+          userDataTattoArtist: {
+            userName: "ShivaShana",
+            password: "hola",
+            email: "email@gmail.com",
+          },
+          professionalDataTattooArtist: {
+            studioName: "Shiva",
+            professionalName: "ShivaShana",
+            phone: 666666666,
+            contactEmail: "email@gmail.com",
+            openingHours: "de 9.00 a 18.00h",
+            direction: "C/hola, nº13",
+            colaboration: "true",
+          },
+        })
+        .expect(201);
+
+      expect(body).toHaveProperty("personalDataTattoArtist", {
+        name: "Gisela",
+        surname1: "Quero",
+        surname2: "Sánchez",
+      });
+    });
+  });
+
+  describe("When it receives a POST request to the /register route with a req.body incorrect", () => {
+    test("Then it should respond a with error and a 404 status and message 'Objeto no válido'", async () => {
+      const { body } = await request
+        .post("/uniteTattoo/tattooArtist/register")
+        .send({
+          personalDataTattoArtisttt: {
+            name: "Gisela",
+            surname1: "Quero",
+            surname2: "Sánchez",
+          },
+          userDataTattoArtist: {
+            userName: "ShivaShana",
+            password: "hola",
+            email: "email@gmail.com",
+          },
+          professionalDataTattooArtist: {
+            studioName: "Shiva",
+            professionalName: "ShivaShana",
+            phone: 666666666,
+            contactEmail: "email@gmail.com",
+            openingHours: "de 9.00 a 18.00h",
+            direction: "C/hola, nº13",
+            colaboration: "true",
+          },
+        })
+        .expect(404);
+
+      expect(body).toHaveProperty("error", "Objeto no válido");
+    });
+  });
+});
