d4d6dd81287ad2f286cde714291c266c5c349d2e
SandraQS <sanquesan@gmail.com>
2021-12-17 15:54:01 +0100

    Add route for tatooArtistPorfile

--- server/routes/tattooArtist/usersRoutes.ts
+++ server/routes/tattooArtist/usersRoutes.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import {
+  tatooArtistPorfile,
   tattooArtistLogin,
   tattooArtistRegister,
 } from "../../controllers/tattooArtistController";
+import auth from "../../middlewares/auth";
 import paths from "../../paths/paths";
 
 const router = express.Router();
 
 router.post(`${paths.register}`, tattooArtistRegister);
 router.post(`${paths.login}`, tattooArtistLogin);
+router.get(`${paths.porfile}`, auth, tatooArtistPorfile);
 
 export default router;
